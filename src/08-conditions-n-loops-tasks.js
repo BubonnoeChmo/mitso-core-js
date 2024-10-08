5270f7e3a6c737dc642d87efad37b18c18dd7156
BubonnoeChmo <morevasnezana9@gmail.com>
2024-10-08 09:59:52 +0300

    86 completed

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -280,8 +280,22 @@ function reverseInteger(num) {
  *   5436468789016589 => false
  *   4916123456789012 => false
  */
-function isCreditCardNumber(/* ccn */) {
-  throw new Error('Not implemented');
+function isCreditCardNumber(ccn) {
+  const ccnStr = ccn.toString();
+  let sum = 0;
+  let alternate = false;
+  for (let i = ccnStr.length - 1; i >= 0; i -= 1) {
+    let currentNum = parseInt(ccnStr.charAt(i), 10);
+    if (alternate) {
+      currentNum *= 2;
+      if (currentNum > 9) {
+        currentNum -= 9;
+      }
+    }
+    sum += currentNum;
+    alternate = !alternate;
+  }
+  return sum % 10 === 0;
 }
 
 /**
