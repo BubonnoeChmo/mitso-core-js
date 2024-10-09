4885ab1b35084aeae355f0f438ea0080ccb47489
BubonnoeChmo <morevasnezana9@gmail.com>
2024-10-09 10:38:33 +0300

    97 completed

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -115,8 +115,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retryWithAttempts() {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        // Обработка ошибок
+      }
+    }
+    throw new Error('All attempts failed');
+  };
 }
 
 /**
